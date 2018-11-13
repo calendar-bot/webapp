4015848be6a0baf5d0de4513efb401402f27c924
Varun Tak <varun.tak@sap.com>
2018-11-13 16:12:47 +0800

    signin/signout toggle on login status check

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivityService } from '../activity.service';
+import {environment} from '../../environments/environment'
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+	notLoggedIn: boolean;
+	loggedIn: boolean;
+	sign_in_url: string;
+	sign_out_url: string;
+
+  constructor(private actService: ActivityService) { }
 
   ngOnInit() {
+  	this.checkIfLoggedIn();
+  	this.setSignInUrl();
+  	this.setSignOutUrl();
+  }
+
+  checkIfLoggedIn() {
+  	this.actService.getLogginStatus().subscribe(status => {this.notLoggedIn = !status;
+  															this.loggedIn = status});
+  }
+
+  setSignInUrl(){
+  	if (environment.integration) {
+  		console.log("Integration login! Switch the environment setting in app to production for productive instance.")
+  		this.sign_in_url = "http://localhost:9000/authorize"
+  	}
+  	else {
+  		this.sign_in_url = "/auth/authorize"
+  	}
+  }
+
+  setSignOutUrl(){
+  	if (environment.integration) {
+  		this.sign_out_url = "http://localhost:9000/clear"
+  	}
+  	else {
+  		this.sign_in_url = "/auth/clear"
+  	}
   }
 
 }
